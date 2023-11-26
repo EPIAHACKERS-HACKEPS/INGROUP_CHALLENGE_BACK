760573ead598c99a03afbb4c2798cf7c4ae02431
Jordi Serrano <44474715+j0rd1s3rr4n0@users.noreply.github.com>
2023-11-26 02:22:20 +0100

    Revert "Update"
    This reverts commit 1eaf00f08211833f98df58efaed4bb3f9803a754.

--- helpers/storages.py
+++ helpers/storages.py
@@ -4,34 +4,22 @@ import os
 from globals import PRIVATE_STORAGE_PATH
 
 class FileStorage(abc.ABC):
-    """
-    Interfaz abstracta para el almacenamiento de archivos.
-    """
+    
     @abc.abstractmethod
     def read(self, name):
-        """Lee el contenido de un archivo."""
         pass
     
     @abc.abstractmethod
     def save(self, file, name):
-        """Guarda el contenido en un archivo."""
         pass
     
     @abc.abstractmethod
     def remove(self, name) -> bool:
-        """Elimina un archivo."""
         pass
     
     def update(self, file, name) -> bool:
-        """
-        Actualiza el contenido de un archivo.
-
-        :param file: Nuevo contenido del archivo.
-        :param name: Nombre del archivo a actualizar.
-        :return: True si la actualización fue exitosa, False si el archivo no existe.
-        """
-        if not self.exists(name):
-            return False
+        
+        if not self.exists(name): return False
         
         self.remove(name)
         self.save(file, name)
@@ -40,12 +28,7 @@ class FileStorage(abc.ABC):
         
     
     def removeAllFiles(self, *names) -> bool:
-        """
-        Elimina varios archivos.
-
-        :param names: Nombres de los archivos a eliminar.
-        :return: True si la eliminación fue exitosa para todos los archivos, False si al menos uno no pudo ser eliminado.
-        """
+        
         allOk = True
         
         if len(names) == 0:
@@ -59,77 +42,36 @@ class FileStorage(abc.ABC):
     
     @abc.abstractmethod
     def removeAll(self):
-        """Elimina todos los archivos."""
         pass
     
     @abc.abstractmethod
     def exists(self, name) -> bool:
-        """Verifica si un archivo existe."""
         pass
     
 class PrivateStorage(FileStorage):
-<<<<<<< Updated upstream
     def __init__(self, path = PRIVATE_STORAGE_PATH):
         self.path = path
-=======
-    """
-    Implementación de FileStorage para almacenamiento privado.
-    """
-    def __init__(self):
-        """
-        Inicializa el almacenamiento privado.
-        """
-        self.path = PRIVATE_STORAGE_PATH
->>>>>>> Stashed changes
         
         if not os.path.exists(self.path):
             os.makedirs(self.path)
         
-    def read(self, name, mode='r'):
-        """
-        Lee el contenido de un archivo.
-
-        :param name: Nombre del archivo a leer.
-        :param mode: Modo de apertura del archivo (por defecto: 'r' para lectura).
-        :return: Contenido del archivo o None si el archivo no existe.
-        """
+    def read(self, name, mode = 'r'):
         if self.exists(name):
             return open(os.path.join(self.path, name), mode)
         else:
             return None
     
-    def save(self, file, name, mode='w'):
-        """
-        Guarda el contenido en un archivo.
-
-        :param file: Contenido a guardar en el archivo.
-        :param name: Nombre del archivo.
-        :param mode: Modo de apertura del archivo (por defecto: 'w' para escritura).
-        """
+    def save(self, file, name, mode = 'w'):
         with open(os.path.join(self.path, name), mode) as f:
             f.write(file.read())
     
-    def write(self, content, name, mode='w'):
-        """
-        Escribe el contenido en un archivo.
-
-        :param content: Contenido a escribir en el archivo.
-        :param name: Nombre del archivo.
-        :param mode: Modo de apertura del archivo (por defecto: 'w' para escritura).
-        :return: True si la escritura fue exitosa, False si el archivo no existe.
-        """
+    def write(self, content, name, mode = 'w'):
         with open(os.path.join(self.path, name), mode) as f:
             f.write(content)
             
         return self.exists(name)
     
     def remove(self, name) -> bool:
-        """
-        Elimina un archivo.
-
-        :param name: Nombre del archivo a eliminar.
-        :return: True si la eliminación fue exitosa, False si el archivo no existe.
-        """
         if self.exists(name):
             os.remove(os.path.join(self.path, name))
             return True
@@ -137,28 +79,17 @@ class PrivateStorage(FileStorage):
         return False
     
     def removeAll(self):
-        """Elimina todos los archivos en el almacenamiento privado."""
-        for filename in os.listdir(self.path):
-            file_path = os.path.join(self.path, filename)
-            os.remove(file_path)
+        os.remove(self.path)
     
     def exists(self, name) -> bool:
-        """
-        Verifica si un archivo existe.
-
-        :param name: Nombre del archivo.
-        :return: True si el archivo existe, False si no.
-        """
         return os.path.exists(os.path.join(self.path, name))
 
+    
 def pdfFile(fun):
-    """
-    Decorador que agrega la extensión '.pdf' al nombre del archivo.
-
-    :param fun: Función a decorar.
-    :return: Función decorada.
-    """
+    
     def wrapper(self, name, *args, **kwargs):
         return fun(self, str(name) + '.pdf', *args, **kwargs)
     
     return wrapper
+    
+    
