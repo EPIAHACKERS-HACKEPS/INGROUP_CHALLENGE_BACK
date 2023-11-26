36a8d42fd93b6c4308de0d067c5259656ad770a6
Daniel Garcia <daniieelgs@gmail.com>
2023-11-26 10:37:34 +0100

    cors

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ def create_app():
     app.config['OPENAPI_URL_PREFIX'] = '/'
     app.config['OPENAPI_SWAGGER_UI_PATH'] = '/swagger-ui'
     app.config['OPENAPI_SWAGGER_UI_URL'] = 'https://cdn.jsdelivr.net/npm/swagger-ui-dist/'
+    app.config['CORS_HEADERS'] = 'Content-Type'
     
     ##BBDD
     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
