950d7b785747b7d524a63614e223454efb707b81
Jordi Serrano <44474715+j0rd1s3rr4n0@users.noreply.github.com>
2023-11-26 08:15:59 +0100

    Update item.py

--- models/item.py
+++ models/item.py
@@ -10,6 +10,6 @@ class ItemModel(db.Model):
     id_userstory = db.Column(db.Integer, db.ForeignKey('user_story.id'), nullable=False)
     item_type_id = db.Column(db.Integer, db.ForeignKey('item_types.id'), nullable=False)
     
-    user_story = db.relationship('UserStoryModel', back_populates='items',ondelete='CASCADE')
+    user_story = db.relationship('UserStoryModel', back_populates='items')
     item_type = db.relationship('ItemTypeModel')
     
